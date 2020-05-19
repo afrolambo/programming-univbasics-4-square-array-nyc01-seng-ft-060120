e23791c4cd40fa066e150a66c801f37b2d671b18
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-19 19:16:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # your code here
   new_numbers = []
-  while array.each do {| item | new_numbers.push(Math.sqrt(item))}
+  while array.each do | item | 
+    new_numbers.push(Math.sqrt(item))
+  end
   new_numbers
 end
