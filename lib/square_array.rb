cbd913525976f3a09e86e4098c47759688d43031
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-19 19:14:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_numbers = []
   while array.length.times do |item| 
-    new_numbers.push(Math.sqrt(array))
+    new_numbers.push(Math.sqrt(item))
   end 
   new_numbers
 end
